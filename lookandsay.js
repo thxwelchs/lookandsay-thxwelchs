63adf1791a3b2d5632702fcbc26fed60d8e817b0
LEE TAE HUN <38197077+thxwelchs@users.noreply.github.com>
2019-09-20 04:09:20 +0900

    [ADD] lookandsay.js

---
+++ lookandsay.js
@@ -0,0 +1,41 @@
+function sequencing(seq) {
+  let temp = "";
+  let len = seq.length;
+  let count = 1;
+  for (let i = 0; i < len; i++) {
+    if (i + 1 !== len && seq.charAt(i) === seq.charAt(i + 1)) count++;
+    else {
+      temp = `${temp}${count}${seq.charAt(i)}`;
+      count = 1;
+    }
+  }
+  return temp;
+}
+
+function lookAndSaySeq(R, L) {
+  if (L === 1) return R;
+
+  for (let i = 0; i < L - 1; i++) {
+    R = sequencing(R);
+  }
+
+  return R;
+}
+
+/** TEST */
+let R = "1";
+let L = 6;
+
+for (let i = 0; i < 5; i++) {
+  if (i > 0) {
+    R = String(~~(Math.random() * (100 - 1) + 1));
+    L = ~~(Math.random() * 25 + 1);
+  }
+
+  console.log(`** ${i + 1}번째 테스트케이스 **`);
+  console.log(
+    `Input\n${R}\n${L}\n\nOutput\n${lookAndSaySeq(R, L)
+      .split("")
+      .join(" ")}`
+  );
+}
